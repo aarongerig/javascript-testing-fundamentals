85eec8ccc34f117927700800e8c57860db0d3782
Aaron Gerig <a.gerig@w-vision.ch>
2019-04-11 15:58:18 +0200

    initial commit and finished course

---
+++ lesson5.js
@@ -0,0 +1,42 @@
+let Pokemons = {
+  amount() {
+    // Call to database
+  },
+
+  say(str) {
+    console.log('Your Pokedex says: ', str);
+  }
+};
+
+function howsMyCollection() {
+  const size = Pokemons.amount();
+
+  if (size === undefined) {
+    return Pokemons.say('Ooops, not sure how many you have');
+  }
+
+  if (size < 10) {
+    return Pokemons.say('You only have a few, you need more');
+  }
+
+  if (size < 50) {
+    return Pokemons.say('You have quite some favorites, keep going');
+  }
+
+  return Pokemons.say('You are quite a collector');
+}
+
+const originalAmount = Pokemons.amount;
+
+function stubAmount(amount) {
+  Pokemons.amount = () => amount;
+}
+
+function havePokemons(amount) {
+  stubAmount(amount);
+  howsMyCollection();
+}
+
+havePokemons(5);
+havePokemons(17);
+havePokemons(100);
